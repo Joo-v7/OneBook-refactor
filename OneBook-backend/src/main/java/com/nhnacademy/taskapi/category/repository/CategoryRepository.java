f8d142589009d4032b933aede21d70acdc717476
joo <helloworld.v7@gmail.com>
2025-08-15 13:37:38 +0900

    spring cahce와 redis 의존성 추가

--- OneBook-backend/src/main/java/com/nhnacademy/taskapi/category/repository/CategoryRepository.java
+++ OneBook-backend/src/main/java/com/nhnacademy/taskapi/category/repository/CategoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -18,7 +19,16 @@ public interface CategoryRepository extends JpaRepository<Category, Integer>, Ca
     // 특정 카테고리의 하위 카테고리 조회
     List<Category> findByParentCategory(Category parentCategory);
 
-    Page<Category> findAll(Pageable pageable);
+    @Query("SELECT c FROM Category c WHERE c.parentCategory IS NULL")
+    List<Category> findRootCategories();
+
+    @Query("SELECT c FROM Category c WHERE c.parentCategory.categoryId = :parentId")
+    List<Category> findByParentId(@Param("parentId") int parentId);
+
+    @Query("select c from Category c")
+    List<Category> findAllForTree();
+
+//    Page<Category> findAll(Pageable pageable);
 
 
 }
